6f1f8e490b12ba50406d56c0d6d616e9a2da0b33
Tung.Nguyen <tung.nguyen@quant-edge.com>
2017-10-08 00:32:22 +0700

    update css

---
+++ EmployeeApp/EmployeeApp/Controller/form/menu.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    //    var hamburger = $('.hamburger');
+    //        hamburger.click(function () {
+    //            hamburger_cross();
+    //        });
+    //        function hamburger_cross() {
+    //    
+    //            if (isClosed == true) {
+    //                hamburger.removeClass('is-open');
+    //                hamburger.addClass('is-closed');
+    //                isClosed = false;
+    //            } else {
+    //                hamburger.removeClass('is-closed');
+    //                hamburger.addClass('is-open');
+    //                isClosed = true;
+    //            }
+    //        }
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+});
